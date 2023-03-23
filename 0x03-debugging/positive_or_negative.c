db670b187a5219648b937ed01f1112d36ce99173
BondSystems <bondsystems@outlook.com>
2023-03-23 06:38:08 -0700

    Update

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- *posi_or_negative - checks for positive or negative numbers
- *
- *@i: the number to be checked
- *Return: always 0
- */
-
+*positive_or_negative.c - checks for positive or negative numbers
+*
+*@i: the number to be checked
+*Return: always 0
+*/
 
 void positive_or_negative(int i)
 {
@@ -14,3 +13,4 @@ void positive_or_negative(int i)
   else
     printf("%d is positive", i);
 }
+
