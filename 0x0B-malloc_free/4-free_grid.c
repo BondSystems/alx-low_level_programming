16281df0e54884daa66389e3863cc61d22a65600
BondSystems <bondsystems@outlook.com>
2023-04-11 18:31:29 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * free_grid - frees 2d array
  * @grid: 2d grid
@@ -10,7 +9,6 @@
  * Return: nothing
  *
  */
-
 void free_grid(int **grid, int height)
 {
 	int i;
@@ -19,6 +17,5 @@ void free_grid(int **grid, int height)
 	{
 		free(grid[i]);
 	}
-
 	free(grid);
 }
